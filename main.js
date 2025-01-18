e0d7834c87675e5774104c09e5d31469df4600bc
Subham Ghosh <isubhamghosh26@gmail.com>
2025-01-19 00:49:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed=new Typed(".text",{
+    strings:["Full-Stack Developer" , "Programmer" , "Content Writer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
